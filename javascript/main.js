df9bf859edc866a3ef397136acde038344e48dc1
Jack Halverson <jhalverson4838@xmail.dixietech.edu>
2024-06-05 11:00:24 -0600

    fix formating

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,38 @@
-function addFun() {
-    const x = parseFloat(document.getElementById('firstNumber').value);
-    const y = parseFloat(document.getElementById('secondNumber').value);
-    alert("answer: " + (x+y))
+function add(x, y) {
+    return x + y;
 }
 
-function multiplyFun() {
-    const x = parseFloat(document.getElementById('firstNumber').value);
-    const y = parseFloat(document.getElementById('secondNumber').value);
-    alert("answer: " + (x*y))
+function multiply(x, y) {
+    return x * y;
 }
 
-function squareFun() {
-    const x = parseFloat(document.getElementById('firstNumber').value);
-    const y = parseFloat(document.getElementById('secondNumber').value);
-    alert("first number squared: " + (x*x))
+function square(x) {
+    return multiply(x, x);
 }
 
-function addSquaresFun() {
+function addSquares(x, y) {
+    return add(square(x), square(y));
+}
+
+function performAddition() {
     const x = parseFloat(document.getElementById('firstNumber').value);
     const y = parseFloat(document.getElementById('secondNumber').value);
-    alert("answer: " + ((x*x)+(y*y)))
+    alert(`${x} + ${y} = ${add(x, y)}`);
+}
+
+function performMultiplication() {
+    const x = parseFloat(document.getElementById('firstNumber').value);
+    const y = parseFloat(document.getElementById('secondNumber').value);
+    alert(`${x} * ${y} = ${multiply(x, y)}`);
+}
+
+function performSquare() {
+    const x = parseFloat(document.getElementById('firstNumber').value);
+    alert(`${x}² = ${square(x)}`);
+}
+
+function performAddSquares() {
+    const x = parseFloat(document.getElementById('firstNumber').value);
+    const y = parseFloat(document.getElementById('secondNumber').value);
+    alert(`(${x}²) + (${y}²) = ${addSquares(x, y)}`);
 }
